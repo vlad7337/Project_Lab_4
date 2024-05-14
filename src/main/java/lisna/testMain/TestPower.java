f1abb1ca210c927a8d6bfb39cd9eb3b497fa961d
vlad3773 <k17vladlsiav@gmail.com>
2024-05-15 02:38:35 +0300

    Update files

--- src/main/java/lisna/testMain/TestPower.java
+++ src/main/java/lisna/testMain/TestPower.java
@@ -1,6 +1,8 @@
 package lisna.testMain;
 
+import lisna.main.Absolute;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import core.main.Linear;
 import core.main.Const;
@@ -12,9 +14,14 @@ public class TestPower {
 
     int x=2;
     int y=3;
-    Power func = Power.of(Const.of(x),Const.of(y));
+    Power func = Power.of(Linear.X,Const.of(y));
+    @BeforeEach
+    void setUp(){
+        func = Power.of(Linear.X,Const.of(y));
+    }
     @Test
     void testCalculate_(){
+        func = Power.of( Const.of(x),Const.of(y));
         double res=func.calculate(x);
         Assertions.assertEquals(8, res);
     }
@@ -22,14 +29,14 @@ public class TestPower {
     @Test
     void testToPrettyString(){
         String result = func.toPrettyString(NumberFormat.getInstance());
-        Assertions.assertEquals(String.format("(%s)^(%s)", x,y),result);
+        Assertions.assertEquals(String.format("(x)^(%s)",y),result);
     }
 
     @Test
     void testDerivative() {
         String result = func.derivative().toPrettyString(NumberFormat.getInstance());
-        String derivativeFunc = Const.of(x).derivative().toPrettyString(NumberFormat.getInstance());
-        Assertions.assertEquals(String.format("(%s*(%s)^((%s-1))*%s)",y,x,y,derivativeFunc), result);
+        String derivativeFunc = Linear.X.derivative().toPrettyString(NumberFormat.getInstance());
+        Assertions.assertEquals(String.format("(%s*(x)^((%s-1))*%s)",y,y,derivativeFunc), result);
     }
 
     @Test
